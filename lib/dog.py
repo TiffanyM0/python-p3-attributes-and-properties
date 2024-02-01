ff5905be64c97179955edc01396fabe3c0a3d69f
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-02-01 23:16:19 +0300

    initial

--- lib/dog.py
+++ lib/dog.py
@@ -13,3 +13,18 @@ APPROVED_BREEDS = [
 
 class Dog:
     pass
+    def __init___(self):
+        self._name = None
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if(type(name) in (str)) and (1 < len(name) < 25):
+            self._name = name
+        else:
+            print("Name nust be string between 1 and 25 characters")
+
+    name = property(get_name, set_name)
+
+fido = Dog()
